0c04a03114e76921bbdd3831e0e6574937170883
Siemens <saimon.kat23@gmail.com>
2021-08-27 16:23:05 +0300

    calc update

--- xln-layout/src/js/calc.js
+++ xln-layout/src/js/calc.js
@@ -1,12 +1,93 @@
-$(() => {
-    const calc = $('.calc');
+const calcInit = () => {
+    const calcEl = document.querySelector('.calc');
     const activeClass = 'xln-active';
 
-    if (calc.length) {
+    if (calcEl) {
+        class Servers {
+            constructor(calcEl) {
+                this.el = calcEl.querySelector('.calc-servers');
+                this.perHour = 0;
+                this.perMonth = 0;
+                this.counter = 0;
+            }
+            total() {
+                this.perHour = this.servers.perHour + 
+                               this.services.perHour + 
+                               this.wans.perHour;
+                this.perMonth = this.servers.perMonth + 
+                                this.services.perMonth + 
+                                this.wans.perMonth;
+            }
+        }
+
+        class Server {
+            constructor(el) {
+                this.el = el;
+                this.perHour = 0;
+                this.perMonth = 0;
+            }
+        }
+
+        class Param {
+            constructor(el) {
+                this.el = el;
+                this.perHour = 0;
+                this.perMonth = 0;
+            }
+        }
+        
+        class Calc {
+            constructor(calcEl, servers) {
+                this.el = calcEl;
+                this.servers = servers;
+                this.perHour = 0;
+                this.perMonth = 0;
+                this.listeners();
+            }
+            listeners() {
+                console.log('listeners');
+                const inputEls = this.el.querySelectorAll('input, select');
+                const calcThis = this;
+                inputEls.forEach(function(inputEl){
+                    inputEl.addEventListener('change', calcThis.update());
+                })
+            }
+            update() {
+                console.log('update');
+            }
+            total() {
+                this.perHour = this.servers.perHour + 
+                               this.services.perHour + 
+                               this.wans.perHour;
+                this.perMonth = this.servers.perMonth + 
+                                this.services.perMonth + 
+                                this.wans.perMonth;
+            }
+        }
+
+        const servers  = new Servers(calcEl);
+        const calc     = new Calc(calcEl, servers);
+        
         // Section toggler
-        $('.calc-section__toggler').click(function() {
-            $(this).toggleClass(activeClass);
-            $(this).parents('.calc-section').find('.calc-section__content').stop().slideToggle();
+        const sectionTogglers = document.querySelectorAll('.calc-section__toggler');
+        sectionTogglers.forEach(function(toggler) {
+            toggler.addEventListener('click', () => {
+                toggler.classList.toggle(activeClass);
+                const parent = toggler.closest('.calc-section');
+                const content = parent.querySelector('.calc-section__content');
+                jQuery(content).stop().slideToggle();
+            })
+        })
+        
+        // Item toggler
+        const itemTogglers = document.querySelectorAll('.calc-item__toggler');
+        itemTogglers.forEach(function(toggler) {
+            toggler.addEventListener('click', () => {
+                const parent = toggler.closest('.calc-item');
+                parent.classList.toggle('.calc-item--disabled');
+            })
         })
     }
-})
+}
+
+window.addEventListener('DOMContentLoaded', calcInit);
