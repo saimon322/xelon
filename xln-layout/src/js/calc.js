4cf99aa651f76c3839c12b20dee032ab8e06e635
Siemens <saimon.kat23@gmail.com>
2021-08-31 10:21:30 +0300

    calc update

--- xln-layout/src/js/calc.js
+++ xln-layout/src/js/calc.js
@@ -24,14 +24,41 @@ const calcInit = () => {
         class Servers {
             constructor(calcEl) {
                 this.el = calcEl.querySelector('.calc-servers');
+                this.container = this.el.querySelector('.calc-section__content');
+                this.items = [];
                 this.perHour = 0;
                 this.perMonth = 0;
                 this.counter = 0;
                 this.init();
             }
             init() {
-                this.items = this.el.querySelectorAll('.calc-server');
-                console.log(this.items);
+                const self = this;
+                this.itemEl = this.el.querySelector('.calc-server').cloneNode(true);
+                self.items.push(new Server(this.itemEl));
+                this.buttons();
+            }
+            buttons() {
+                const self = this;
+                this.el.addEventListener('click', function(e){
+                    if(e.target && e.target.classList.contains('calc-block__button--add')){
+                        self.addItem();
+                    } else if(e.target && e.target.classList.contains('calc-block__button--remove')){
+                        self.removeItem(e.target);
+                    }
+                });
+            }
+            addItem() {
+                const clone = this.itemEl.cloneNode(true);
+                this.container.appendChild(clone);
+                this.items.push(new Server(clone));
+                const selects = clone.querySelectorAll('.calc-select');
+                selectsInit(selects);
+            }
+            removeItem(item) {
+                const server = item.closest('.calc-server');
+                const index = [...server.parentNode.children].indexOf(server);
+                this.items.splice(index, 1);
+                server.remove();
             }
             total() {
                 this.perHour = this.servers.perHour + 
@@ -52,11 +79,6 @@ const calcInit = () => {
                 this.init();
             }
             init() {
-                const inputEls = this.el.querySelectorAll('input');
-                const calcThis = this;
-                inputEls.forEach(function(inputEl){
-                    inputEl.addEventListener('change', calcThis.update());
-                })
             }
             update() {
                 console.log('update');
@@ -75,48 +97,71 @@ const calcInit = () => {
         const calc     = new Calc(calcEl, servers);
 
         // Section toggler
-        const sectionTogglers = document.querySelectorAll('.calc-section__toggler');
-        sectionTogglers && sectionTogglers.forEach(function(toggler) {
-            toggler.addEventListener('click', () => {
+        calcEl.addEventListener('click', function(e){
+            if(e.target && e.target.classList.contains('calc-section__toggler')){
+                toggler = e.target;
                 toggler.classList.toggle(activeClass);
                 const parent = toggler.closest('.calc-section');
                 const content = parent.querySelector('.calc-section__content');
                 jQuery(content).stop().slideToggle();
-            })
+            }
         })
         
         // Item toggler
-        const itemTogglers = document.querySelectorAll('.calc-item__toggler');
-        itemTogglers && itemTogglers.forEach(function(toggler) {
-            toggler.addEventListener('click', () => {
+        calcEl.addEventListener('click', function(e){
+            if(e.target && e.target.classList.contains('calc-item__toggler')){
+                toggler = e.target;
                 const parent = toggler.closest('.calc-item');
                 parent.classList.toggle('calc-item--disabled');
-            })
+            }
+        })
+
+        // Counter     
+        calcEl.addEventListener('input', function(e){
+            if(e.target && e.target.classList.contains('calc-input')){
+                calc.update();
+            }
+        });
+        calcEl.addEventListener('click', function(e){
+            if(e.target && e.target.classList.contains('calc-counter__button')){
+                const button = e.target;
+                const input = button.parentElement.querySelector('.calc-counter__input');
+                if (button.classList.contains('calc-minus')) {
+                    input.stepDown();
+                } else if (button.classList.contains('calc-plus')) {
+                    input.stepUp();
+                }
+            }
         })
 
         // Select
-        const selects = document.querySelectorAll('.calc-select');
-        selects && selects.forEach(function(select) {
-            const items = select.querySelectorAll('.calc-select__item');
-            const current = select.querySelector('.calc-select__current');
-            current.innerHTML = items[0].innerHTML;
-            setDataAttributes(items[0], current);
+        const selects = calcEl.querySelectorAll('.calc-select');
+        selectsInit(selects);
 
-            select.addEventListener('mouseover', () => {
-                select.classList.add(activeClass);
-            })
-            select.addEventListener('mouseout', () => {
-                select.classList.remove(activeClass);
-            })
-            
-            items && items.forEach(function(item) {
-                item.addEventListener('click', () => {
-                    current.innerHTML = item.innerHTML;
-                    setDataAttributes(item, current);
+        function selectsInit(selects) {
+            selects && selects.forEach(function(select) {
+                const items = select.querySelectorAll('.calc-select__item');
+                const current = select.querySelector('.calc-select__current');
+                current.innerHTML = items[0].innerHTML;
+                setDataAttributes(items[0], current);
+    
+                select.addEventListener('mouseover', () => {
+                    select.classList.add(activeClass);
+                })
+                select.addEventListener('mouseout', () => {
                     select.classList.remove(activeClass);
                 })
+                
+                items && items.forEach(function(item) {
+                    item.addEventListener('click', () => {
+                        current.innerHTML = item.innerHTML;
+                        setDataAttributes(item, current);
+                        select.classList.remove(activeClass);
+                        calc.update();
+                    })
+                })
             })
-        })
+        }
     }
 }
 
