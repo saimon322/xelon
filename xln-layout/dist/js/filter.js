d57c0e34ae27450add49746da756651a2380f13b
Saimon Kat <saimon.kat23@gmail.com>
2021-11-22 02:30:38 +0300

    gitignore dist js

---
+++ xln-layout/dist/js/filter.js
@@ -0,0 +1,58 @@
+(function ($) {
+
+    $('.xln-news__tag').click(function (e) {
+
+        $('.xln-news__tag.xln-active').removeClass('xln-active');
+        $(this).addClass('xln-active');
+
+        let cat = $(this).data('cat');
+
+        load_posts(cat, 1);
+
+    });
+
+    function pagination_init() {
+        $('button.xln-pagination__page, .xln-pagination__arrow').click(function () {
+            let cat = $('.xln-news__tag.xln-active').data('cat');
+            let paged = $(this).data('paged');
+
+            load_posts(cat, paged);
+        });
+    }
+
+    function load_posts(cat, paged) {
+        if (!$('body').hasClass('loading')) {
+            
+            let posts_per_page = 3;
+            
+            if ($('body').hasClass('category')){
+                posts_per_page = 12;
+            }
+            
+            $.ajax({
+                url: filter.ajax_url,
+                data: {
+                    action: filter.action,
+                    nonce: filter.nonce,
+                    category: cat,
+                    paged: paged,
+                    posts_per_page: posts_per_page,
+                },
+                dataType: 'json',
+                type: 'POST',
+                beforeSend: function (xhr) {
+                    $('body').addClass('loading');
+                },
+                success: function (data) {
+                    $('body').removeClass('loading');
+                    $('.xln-news__content').html(data.posts);
+                    $('.xln-news__pagination.xln-pagination').html(data.pagination);
+                    pagination_init();
+                }
+            });
+        }
+    }
+
+    pagination_init();
+
+})(jQuery)
